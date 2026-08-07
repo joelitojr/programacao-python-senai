cc21f826e46f51fa24254f7dd1131b84cd573934
joelitojr <joelitojr@gmail.com>
2026-08-07 19:56:53 -0300

    Add files via upload

---
+++ Aula-16/main_pygame.py
@@ -0,0 +1,325 @@
+# # https://www.pygame.org/docs/
+# # quora.com
+
+
+# # 05/08/2026
+
+# # Exemplo da professora 
+
+import pygame
+pygame.init()
+
+janela  =  pygame.display.set_mode([500,500])
+run  =  True
+
+while run:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            run  =  False
+
+
+# # 06/08/2026
+
+# # Exemplos da professora 
+
+# # 1
+import pygame
+pygame.init()
+
+tela  =  pygame.display.set_mode([500,500])
+pygame.display.set_caption("TESTANDO PYGAME")
+
+clock =  pygame.time.Clock()
+
+run =  True
+
+while run:
+    for evento  in pygame.event.get():
+        if evento.type  == pygame.QUIT: 
+            run = False
+    tela.fill('blue')
+    pygame.draw.rect(tela, 'white',(100,100,50,50))
+    pygame.draw.circle(tela, 'yellow',(250,250), 100)
+    pygame.display.update()   
+
+
+# # 2
+import pygame
+pygame.init()
+
+tela  =  pygame.display.set_mode([500,500])
+pygame.display.set_caption("TESTANDO PYGAME")
+# cir  =  draw.Circle(tela, 'yellow',(250,250), 100)
+clock =  pygame.time.Clock()
+run =  True
+while run:
+    for evento  in pygame.event.get():
+        if evento.type  == pygame.QUIT: 
+            run = False
+        tela.fill('blue')
+    
+        cir  =  pygame.draw.circle(tela, 'yellow',(250,250), 100)
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_RIGHT:
+                pygame.draw.rect(tela, 'white',(350,100,50,50))
+                cir.x  =  cir.x  + 20
+            if evento.key == pygame.K_LEFT:
+                pygame.draw.rect(tela, 'green',(150,100,50,50)) 
+            if evento.key == pygame.K_DOWN:
+                pygame.draw.rect(tela, 'purple',(30,350,50,50))       
+            if evento.key == pygame.K_UP:
+                pygame.draw.rect(tela, 'yellow',(380,400,50,50)) 
+
+    # keys = pygame.key.get_pressed()
+    # if keys['K_RIGHT ']:
+    #    pass
+    pygame.display.update()   
+
+
+# # Exercícios
+
+# # 1 - JOGO DO TREX ( Modelo Google ) SEM SOM   
+import pygame
+import sys
+import random
+pygame.init()
+
+LARGURA   = 800
+ALTURA    = 400
+tela      = pygame.display.set_mode([LARGURA, ALTURA])
+
+pygame.display.set_caption('JOGO TREX')
+
+# CARREGAR AS IMAGENS
+
+trex1     = pygame.image.load('trex1.png')
+trex2     = pygame.image.load('trex4.png')
+cacto_img = pygame.image.load('obstacle1.png')
+# cacto1  = pygame.image.load('obstacle1.png')
+# cacto2  = pygame.image.load('obstacle2.png')
+chao      = pygame.image.load('ground2.png')
+
+trex_x    = 100
+trex_y    = 300
+
+vel_y     = 0
+gravidade = 1
+pulando   = False 
+chao_x    = 0
+cacto_x   = 800
+cacto_y   = 300
+score     = 0
+frame     = 0
+
+fonte     = pygame.font.SysFont("Arial", 30, bold=True)
+
+game_over = False 
+clock     = pygame.time.Clock()
+
+while True:
+    # Captura de eventos do Jogo ....
+    for evento in pygame.event.get():
+        if evento.type    ==  pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if evento.type    ==  pygame.KEYDOWN:
+            if evento.key ==  pygame.K_SPACE and not pulando:
+                vel_y      = -18
+                pulando    =  True 
+            if evento.key ==  pygame.K_w     and     game_over:
+                trex_y     =  300
+                cacto_x    =  800
+                score      =  0
+                game_over  =  False
+# ------------------------------------------------------------
+    if not game_over:
+        vel_y         +=  gravidade
+        trex_x        +=  vel_y 
+
+        if trex_y     >=  300:
+            trex_y     =  300
+            pulando    =  False
+
+        chao_x        -=  5
+        if chao_x      < -800:
+            chao_x     =  0 
+
+        cacto_x       -=  5
+        if cacto_x     < -50:
+            cacto_x    =  random.randint(800,1000)
+            score     +=  1
+
+        frame         +=  1
+        if frame      >=  20:
+            frame      =  0
+        if frame       <  10:
+            trex       =  trex1
+        else:
+            trex       =  trex2
+
+        trex_rect      =  trex.get_rect     (topleft = (trex_x, trex_y  ))
+        cacto_rect     =  cacto_img.get_rect(topleft = (cacto_x, cacto_y))
+
+        if trex_rect.collidedict(cacto_rect):
+            game_over  = True
+
+    tela.fill(('yellow'))
+
+    tela.blit(chao,      (chao_x, 340))
+    
+    tela.blit(chao,      (+800, 340))
+
+    tela.blit(trex,      (trex_x, trex_y))
+
+    tela.blit(cacto_img, (cacto_x, cacto_y))
+
+
+    texto = fonte.render('PONTOS: ', str(score), True)
+    tela.blit(texto, (600,20))
+
+    if game_over:
+        texto2 = fonte.render('GAME OVER', str(score), True)
+        tela.blit(texto2, (310,400))
+
+    pygame.display.update()
+    clock.tick(30)
+
+
+# # 1.1 - JOGO DO TREX ( Modelo Google ) COM SOM   
+
+
+
+# # 07/08/2026
+
+# # Exemplo da professora 
+
+# # 1 - 
+import pygame 
+import sys
+import random
+
+pygame.init()
+LARGURA =  800
+ALTURA  =  400
+tela  =  pygame.display.set_mode([LARGURA, ALTURA])
+
+pygame.display.set_caption('JOGO TREX')
+# CARREGAR AS IMAGENS 
+
+
+t  =  (20,20)
+trex1 = pygame.image.load('1.png')
+picture = pygame.transform.scale(trex1, (60, 60))
+
+trex2 = pygame.image.load('2.png')
+picture2 = pygame.transform.scale(trex2, (60, 60))
+
+trex3 = pygame.image.load('3.png')
+picture3 = pygame.transform.scale(trex2, (60, 60))
+
+
+cacto_img = pygame.image.load('obstacle1.png')
+# cacto2 = pygame.image.load('obstacle2.png') 
+chao = pygame.image.load('ground2.png')
+# msc = pygame.mixer_music.load('arca.mp3')
+# msc2 =  pygame.mixer_music.load('go.mp3')
+
+trex_x = 100
+trex_y = 300
+
+vel_y =  0
+gravidade = 1
+pulando = False
+chao_x = 0
+cacto_x = 800
+cacto_y = 300
+score =  0
+
+frame =  0
+
+fonte = pygame.font.SysFont("Arial", 30, bold=True)
+
+game_over  = False 
+clock = pygame.time.Clock()
+
+
+while True:
+    # captura de eventos no jogo .... 
+
+    pygame.mixer.init()
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_SPACE and not pulando:
+               vel_y = -20
+               pulando = True
+            if  evento.key == pygame.K_w and game_over: 
+                trex_y = 300
+                cacto_x = 800
+                score = 0
+                game_over = False
+# -------------------------------------------------------
+    if not game_over:
+
+        vel_y += gravidade
+        trex_y += vel_y
+        # pygame.mixer.init()
+        # pygame.mixer.music.load('arca.mp3')
+        # pygame.mixer.music.play()
+        
+        if  trex_y >= 300:
+            # pygame.mixer.music.play()
+            trex_y = 300
+            pulando  = False
+
+        chao_x -=  5
+        if chao_x <= -800:
+           chao_x = 0
+
+        cacto_x -= 5
+        if cacto_x < -50 :
+            cacto_x = random.randint(800,1000)  
+            score = score +  1
+
+        frame += 1
+        if frame > 30:
+            frame = 0
+            
+        if frame < 10:
+            trex = picture
+
+        elif frame >= 15 and frame <= 30:
+            trex = picture2
+
+        else:
+            trex = picture3           
+
+        trex_rect  =  trex.get_rect(topleft = (trex_x, trex_y))
+        cacto_rect  = cacto_img.get_rect(topleft = (cacto_x, cacto_y))
+
+        if trex_rect.colliderect(cacto_rect):
+            # pygame.mixer.music.play(msc2)
+            game_over = True
+
+    tela.fill(('yellow'))
+
+    tela.blit(chao,(chao_x, 340))    
+
+    tela.blit(chao,(chao_x+800,340))
+
+    tela.blit(trex, (trex_x, trex_y))
+
+    tela.blit(cacto_img, (cacto_x, cacto_y))
+
+    texto = fonte.render('PONTOS:'+ str(score), True, 'red')
+
+    tela.blit(texto, (600,20))
+
+    if game_over:
+        texto2 =  fonte.render('GAME OVER', str(score), True)
+        tela.blit(texto2, (310,400))
+          
+    pygame.display.update()
+    clock.tick(30)					
